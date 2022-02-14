d5c1c914f9deb99fcff89514845d43c23684adef
alfercar <alfonsofdez97@gmail.com>
2022-02-14 19:31:33 +0100

    cv

--- CV.js
+++ CV.js
@@ -6,3 +6,71 @@ botonito.addEventListener("click", showMessage)
 function showMessage (){
     alert("Vaya vaya...")
 }
+
+
+
+
+const mostrar = document.getElementById('botoninfo');
+
+mostrar.addEventListener('click', masinfo);
+
+function masinfo(){    
+    document.getElementById('todaInfo').classList.toggle('oculto');         
+};
+
+
+
+
+const mostrar2 = document.getElementById('botoninfo2');
+
+mostrar2.addEventListener('click', masinfo2);
+
+function masinfo2(){    
+    document.getElementById('todaInfo2').classList.toggle('oculto');         
+};
+
+
+
+
+
+const mostrar3 = document.getElementById('botoninfo3');
+
+mostrar3.addEventListener('click', masinfo3);
+
+function masinfo3(){    
+    document.getElementById('todaInfo3').classList.toggle('oculto');         
+};
+
+
+
+
+
+const mostrar4 = document.getElementById('botoninfo4');
+
+mostrar4.addEventListener('click', masinfo4);
+
+function masinfo4(){    
+    document.getElementById('todaInfo4').classList.toggle('oculto');         
+};
+
+
+
+
+const mostrar5 = document.getElementById('botoninfo5');
+
+mostrar5.addEventListener('click', masinfo5);
+
+function masinfo5(){    
+    document.getElementById('todaInfo5').classList.toggle('oculto');         
+};
+
+
+
+
+const mostrar6 = document.getElementById('botoninfo6');
+
+mostrar6.addEventListener('click', masinfo6);
+
+function masinfo6(){    
+    document.getElementById('todaInfo6').classList.toggle('oculto');         
+};
